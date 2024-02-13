9d3ee15e05edabbd642cd73db7e0cff7f6f33b01
KimSang-Ui <155289035+KimSang-Ui@users.noreply.github.com>
2024-02-14 01:09:49 +0900

    Add files via upload
    C# 파파존스 게시판 만들기

---
+++ _posts/FirstCustomControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp2_파파존스_게시판_
+{
+    public partial class FirstCustomControl : UserControl
+    {
+        public FirstCustomControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
