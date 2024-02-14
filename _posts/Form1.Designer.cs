27d7d83f0be40a561da72f081aa66825a992dec4
KimSang-Ui <155289035+KimSang-Ui@users.noreply.github.com>
2024-02-14 22:59:38 +0900

    Add files via upload
    스탑워치 코드리뷰 자료

--- _posts/Form1.Designer.cs
+++ _posts/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WinFormsApp2_파파존스_게시판_
+﻿namespace WinFormsApp_코드리뷰_스탑워치_
 {
     partial class Form1
     {
@@ -28,350 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            panel1 = new Panel();
-            SidePanel = new Panel();
-            button7 = new Button();
-            button6 = new Button();
-            button5 = new Button();
-            button4 = new Button();
-            button3 = new Button();
-            button2 = new Button();
-            button1 = new Button();
-            panel2 = new Panel();
-            panel3 = new Panel();
-            label2 = new Label();
-            label3 = new Label();
-            pictureBox1 = new PictureBox();
+            components = new System.ComponentModel.Container();
             label1 = new Label();
-            button8 = new Button();
-            button9 = new Button();
-            button10 = new Button();
-            button11 = new Button();
-            button12 = new Button();
-            firstCustomControl2 = new FirstCustomControl();
-            mySecondCustomControl1 = new MySecondCustomControl();
-            thirdCustomControl11 = new thirdCustomControl1();
-            panel1.SuspendLayout();
-            panel3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            label2 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            button4 = new Button();
             SuspendLayout();
-            // 
-            // panel1
-            // 
-            panel1.BackColor = Color.FromArgb(41, 39, 40);
-            panel1.Controls.Add(SidePanel);
-            panel1.Controls.Add(button7);
-            panel1.Controls.Add(button6);
-            panel1.Controls.Add(button5);
-            panel1.Controls.Add(button4);
-            panel1.Controls.Add(button3);
-            panel1.Controls.Add(button2);
-            panel1.Controls.Add(button1);
-            panel1.Dock = DockStyle.Left;
-            panel1.Location = new Point(0, 0);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(171, 551);
-            panel1.TabIndex = 0;
-            // 
-            // SidePanel
-            // 
-            SidePanel.BackColor = Color.FromArgb(178, 8, 55);
-            SidePanel.ForeColor = SystemColors.ControlText;
-            SidePanel.Location = new Point(12, 27);
-            SidePanel.Name = "SidePanel";
-            SidePanel.Size = new Size(10, 54);
-            SidePanel.TabIndex = 3;
-            // 
-            // button7
-            // 
-            button7.BackColor = Color.FromArgb(41, 39, 40);
-            button7.Font = new Font("맑은 고딕", 10.8F, FontStyle.Bold);
-            button7.ForeColor = Color.White;
-            button7.Location = new Point(12, 489);
-            button7.Name = "button7";
-            button7.Size = new Size(153, 46);
-            button7.TabIndex = 3;
-            button7.Text = "Collection";
-            button7.TextAlign = ContentAlignment.MiddleLeft;
-            button7.TextImageRelation = TextImageRelation.ImageBeforeText;
-            button7.UseVisualStyleBackColor = false;
-            // 
-            // button6
-            // 
-            button6.BackColor = Color.FromArgb(41, 39, 40);
-            button6.Font = new Font("맑은 고딕", 10.8F, FontStyle.Bold);
-            button6.ForeColor = Color.White;
-            button6.Location = new Point(12, 412);
-            button6.Name = "button6";
-            button6.Size = new Size(156, 46);
-            button6.TabIndex = 3;
-            button6.Text = "Driver Payment";
-            button6.TextAlign = ContentAlignment.MiddleLeft;
-            button6.TextImageRelation = TextImageRelation.ImageBeforeText;
-            button6.UseVisualStyleBackColor = false;
-            // 
-            // button5
-            // 
-            button5.BackColor = Color.FromArgb(41, 39, 40);
-            button5.Font = new Font("맑은 고딕", 10.8F, FontStyle.Bold);
-            button5.ForeColor = Color.White;
-            button5.Location = new Point(12, 335);
-            button5.Name = "button5";
-            button5.Size = new Size(156, 46);
-            button5.TabIndex = 3;
-            button5.Text = "Take Away";
-            button5.TextAlign = ContentAlignment.MiddleLeft;
-            button5.TextImageRelation = TextImageRelation.ImageBeforeText;
-            button5.UseVisualStyleBackColor = false;
-            // 
-            // button4
-            // 
-            button4.BackColor = Color.FromArgb(41, 39, 40);
-            button4.Font = new Font("맑은 고딕", 10.8F, FontStyle.Bold);
-            button4.ForeColor = Color.White;
-            button4.Location = new Point(12, 258);
-            button4.Name = "button4";
-            button4.Size = new Size(156, 46);
-            button4.TabIndex = 3;
-            button4.Text = "Delievery";
-            button4.TextAlign = ContentAlignment.MiddleLeft;
-            button4.TextImageRelation = TextImageRelation.ImageBeforeText;
-            button4.UseVisualStyleBackColor = false;
-            // 
-            // button3
-            // 
-            button3.BackColor = Color.FromArgb(41, 39, 40);
-            button3.Font = new Font("맑은 고딕", 10.8F, FontStyle.Bold);
-            button3.ForeColor = Color.White;
-            button3.Location = new Point(12, 181);
-            button3.Name = "button3";
-            button3.Size = new Size(156, 46);
-            button3.TabIndex = 3;
-            button3.Text = "Event/Sale";
-            button3.TextAlign = ContentAlignment.MiddleLeft;
-            button3.TextImageRelation = TextImageRelation.ImageBeforeText;
-            button3.UseVisualStyleBackColor = false;
-            button3.Click += button3_Click;
-            // 
-            // button2
-            // 
-            button2.BackColor = Color.FromArgb(41, 39, 40);
-            button2.Font = new Font("맑은 고딕", 10.8F, FontStyle.Bold);
-            button2.ForeColor = Color.White;
-            button2.Location = new Point(12, 104);
-            button2.Name = "button2";
-            button2.Size = new Size(156, 46);
-            button2.TabIndex = 3;
-            button2.Text = "Eat-in";
-            button2.TextAlign = ContentAlignment.MiddleLeft;
-            button2.TextImageRelation = TextImageRelation.ImageBeforeText;
-            button2.UseVisualStyleBackColor = false;
-            button2.Click += button2_Click;
-            // 
-            // button1
-            // 
-            button1.BackColor = Color.FromArgb(41, 39, 40);
-            button1.Font = new Font("맑은 고딕", 10.8F, FontStyle.Bold);
-            button1.ForeColor = Color.White;
-            button1.Location = new Point(12, 27);
-            button1.Name = "button1";
-            button1.Size = new Size(153, 46);
-            button1.TabIndex = 3;
-            button1.Text = "Home";
-            button1.TextAlign = ContentAlignment.MiddleLeft;
-            button1.TextImageRelation = TextImageRelation.ImageBeforeText;
-            button1.UseVisualStyleBackColor = false;
-            button1.Click += button1_Click;
-            // 
-            // panel2
-            // 
-            panel2.BackColor = Color.FromArgb(178, 8, 55);
-            panel2.Dock = DockStyle.Top;
-            panel2.Location = new Point(171, 0);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(971, 31);
-            panel2.TabIndex = 1;
-            // 
-            // panel3
-            // 
-            panel3.BackColor = Color.FromArgb(178, 8, 55);
-            panel3.Controls.Add(label2);
-            panel3.Controls.Add(label3);
-            panel3.Controls.Add(pictureBox1);
-            panel3.Location = new Point(243, 0);
-            panel3.Name = "panel3";
-            panel3.Size = new Size(205, 153);
-            panel3.TabIndex = 2;
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Font = new Font("맑은 고딕", 12F, FontStyle.Bold, GraphicsUnit.Point, 129);
-            label2.ForeColor = Color.White;
-            label2.Location = new Point(16, 88);
-            label2.Name = "label2";
-            label2.Size = new Size(186, 28);
-            label2.TabIndex = 0;
-            label2.Text = "Better Ingredients";
-            label2.TextAlign = ContentAlignment.MiddleCenter;
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new Font("맑은 고딕", 12F, FontStyle.Bold, GraphicsUnit.Point, 129);
-            label3.ForeColor = Color.White;
-            label3.Location = new Point(37, 116);
-            label3.Name = "label3";
-            label3.Size = new Size(126, 28);
-            label3.TabIndex = 0;
-            label3.Text = "Better Pizza";
-            label3.TextAlign = ContentAlignment.MiddleCenter;
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(3, 20);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(199, 61);
-            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox1.TabIndex = 3;
-            pictureBox1.TabStop = false;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("맑은 고딕", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 129);
-            label1.Location = new Point(454, 40);
+            label1.Font = new Font("Impact", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(43, 28);
             label1.Name = "label1";
-            label1.Size = new Size(272, 23);
-            label1.TabIndex = 3;
-            label1.Text = "C# UI Designing By KIM SANG-UI";
+            label1.Size = new Size(157, 28);
+            label1.TabIndex = 0;
+            label1.Text = "C# - StopWatch";
             // 
-            // button8
+            // label2
             // 
-            button8.Image = Properties.Resources.스크린샷_2024_02_12_005946;
-            button8.Location = new Point(722, 37);
-            button8.Name = "button8";
-            button8.Size = new Size(82, 89);
-            button8.TabIndex = 4;
-            button8.TextImageRelation = TextImageRelation.ImageBeforeText;
-            button8.UseVisualStyleBackColor = true;
+            label2.AutoSize = true;
+            label2.Font = new Font("Impact", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(208, 142);
+            label2.Name = "label2";
+            label2.Size = new Size(147, 42);
+            label2.TabIndex = 0;
+            label2.Text = "0 : 0 : 0 : 0";
             // 
-            // button9
+            // button1
             // 
-            button9.Image = (Image)resources.GetObject("button9.Image");
-            button9.Location = new Point(810, 37);
-            button9.Name = "button9";
-            button9.Size = new Size(82, 89);
-            button9.TabIndex = 4;
-            button9.TextImageRelation = TextImageRelation.ImageBeforeText;
-            button9.UseVisualStyleBackColor = true;
+            button1.BackColor = SystemColors.ActiveBorder;
+            button1.Font = new Font("Impact", 12F);
+            button1.ForeColor = SystemColors.ActiveCaptionText;
+            button1.Location = new Point(43, 265);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 39);
+            button1.TabIndex = 1;
+            button1.Text = "Start";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
             // 
-            // button10
+            // button2
             // 
-            button10.Image = (Image)resources.GetObject("button10.Image");
-            button10.Location = new Point(898, 37);
-            button10.Name = "button10";
-            button10.Size = new Size(82, 89);
-            button10.TabIndex = 4;
-            button10.TextImageRelation = TextImageRelation.ImageBeforeText;
-            button10.UseVisualStyleBackColor = true;
+            button2.BackColor = SystemColors.ActiveBorder;
+            button2.Font = new Font("Impact", 12F);
+            button2.ForeColor = SystemColors.ActiveCaptionText;
+            button2.Location = new Point(239, 265);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 39);
+            button2.TabIndex = 1;
+            button2.Text = "Stop";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
             // 
-            // button11
+            // button3
             // 
-            button11.Image = (Image)resources.GetObject("button11.Image");
-            button11.Location = new Point(986, 37);
-            button11.Name = "button11";
-            button11.Size = new Size(72, 64);
-            button11.TabIndex = 4;
-            button11.TextImageRelation = TextImageRelation.ImageBeforeText;
-            button11.UseVisualStyleBackColor = true;
+            button3.BackColor = SystemColors.ActiveBorder;
+            button3.Font = new Font("Impact", 12F);
+            button3.ForeColor = SystemColors.ActiveCaptionText;
+            button3.Location = new Point(435, 265);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 39);
+            button3.TabIndex = 1;
+            button3.Text = "Reset";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
             // 
-            // button12
+            // timer1
             // 
-            button12.Image = (Image)resources.GetObject("button12.Image");
-            button12.Location = new Point(1064, 37);
-            button12.Name = "button12";
-            button12.Size = new Size(72, 64);
-            button12.TabIndex = 4;
-            button12.TextImageRelation = TextImageRelation.ImageBeforeText;
-            button12.UseVisualStyleBackColor = true;
+            timer1.Tick += timer1_Tick;
             // 
-            // firstCustomControl2
+            // button4
             // 
-            firstCustomControl2.Location = new Point(230, 147);
-            firstCustomControl2.Name = "firstCustomControl2";
-            firstCustomControl2.Size = new Size(867, 404);
-            firstCustomControl2.TabIndex = 5;
-            // 
-            // mySecondCustomControl1
-            // 
-            mySecondCustomControl1.Location = new Point(195, 169);
-            mySecondCustomControl1.Name = "mySecondCustomControl1";
-            mySecondCustomControl1.Size = new Size(863, 370);
-            mySecondCustomControl1.TabIndex = 6;
-            // 
-            // thirdCustomControl11
-            // 
-            thirdCustomControl11.Location = new Point(177, 150);
-            thirdCustomControl11.Name = "thirdCustomControl11";
-            thirdCustomControl11.Size = new Size(887, 389);
-            thirdCustomControl11.TabIndex = 7;
+            button4.BackColor = SystemColors.ActiveBorder;
+            button4.Font = new Font("Impact", 12F);
+            button4.ForeColor = SystemColors.ActiveCaptionText;
+            button4.Location = new Point(435, 28);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 29);
+            button4.TabIndex = 2;
+            button4.Text = "Alarm";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1142, 551);
-            Controls.Add(thirdCustomControl11);
-            Controls.Add(mySecondCustomControl1);
-            Controls.Add(firstCustomControl2);
-            Controls.Add(button12);
-            Controls.Add(button11);
-            Controls.Add(button10);
-            Controls.Add(button9);
-            Controls.Add(button8);
+            ClientSize = new Size(800, 450);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label2);
             Controls.Add(label1);
-            Controls.Add(panel3);
-            Controls.Add(panel2);
-            Controls.Add(panel1);
-            FormBorderStyle = FormBorderStyle.None;
             Name = "Form1";
-            StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
-            panel1.ResumeLayout(false);
-            panel3.ResumeLayout(false);
-            panel3.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Panel panel1;
-        private Panel panel2;
-        private Panel panel3;
-        private PictureBox pictureBox1;
-        private Button button1;
-        private Label label2;
-        private Label label3;
-        private Button button7;
-        private Button button6;
-        private Button button5;
-        private Button button4;
-        private Button button3;
-        private Button button2;
-        private Panel SidePanel;
         private Label label1;
-        private Button button8;
-        private Button button9;
-        private Button button10;
-        private Button button11;
-        private Button button12;
-        private FirstCustomControl firstCustomControl2;
-        private MySecondCustomControl mySecondCustomControl1;
-        private thirdCustomControl1 thirdCustomControl11;
+        private Label label2;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private Button button4;
     }
 }
