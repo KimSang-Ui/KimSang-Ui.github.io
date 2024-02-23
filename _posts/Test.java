ba9575484c43285217b128f05ada16b5a04043f3
KimSang-Ui <k93158612@gmail.com>
2024-02-23 23:39:25 +0900

    20240223

---
+++ _posts/Test.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Test {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int dan= sc.nextInt();
+      
+     
+        }
+
+
+        /*
+         * 사용자로부터 몇단?
+         * 3단
+         * 
+         * 3*1
+         * 3*3
+         * 3*5
+         * 3*7
+         * 3*9
+         * 
+         */
+
+         /*
+          * 몇개의 별 출력할래?
+          3
+          *
+          **
+          ***
+          4
+          *
+          **
+          ***
+          ****
+          */
+
+        // ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(10, 20, 30, 40, 50));
+        // System.out.println(arr);
+        // System.out.println(arr.get(4));
+        // System.out.println(arr.get(3));
+        // System.out.println(arr.get(2));
+        // System.out.println(arr.get(1));
+        // System.out.println(arr.get(0));
+
+        // for (int i = 0; i < 5 ; i++) {
+        // System.out.print(arr.get(i)+" ");
+
+        // }
+
+        // int arr[] = { 1, 2, 3, 4, 5 };
+
+        // for (int i = 0; i < 5; i++) {
+        //     if (i % 2 != 0) {
+        //         System.out.println(i);
+        //     }
+        // }
+    }
+
+}
