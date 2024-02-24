19e3732a0d341a6b35adb0ab9447138d87919300
KimSang-Ui <k93158612@gmail.com>
2024-02-24 23:45:42 +0900

    240224

--- _posts/Test.java
+++ _posts/Test.java
@@ -16,27 +16,21 @@
 //         }
 // }
 
-class Star3 {
-        public static void main(String[] args) {
-                for(int i = 1; i <= 3; i++) {
-                
-                        for(int j = 1; j <= i; j++) {
-                            System.out.print("*");
-                        }
-                        System.out.println("");
-                    }
+import java.util.Scanner;
 
-}
-}
-class Star4 {
-        public static void main(String[] args) {
-                for(int i = 1; i <= 4; i++) {
-                
-                        for(int j = 1; j <= i; j++) {
-                            System.out.print("*");
-                        }
-                        System.out.println("");
-                    }
+class Star {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++){ 
+                System.out.print("*");
+
+            }
+        System.out.println();
+        
+
+    }
 
 }
 }
