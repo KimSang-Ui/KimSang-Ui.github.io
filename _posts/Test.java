2bc022e9b95718ae1109ff605becd811c7680379
KimSang-Ui <k93158612@gmail.com>
2024-02-27 00:10:57 +0900

    240227

--- _posts/Test.java
+++ _posts/Test.java
@@ -18,7 +18,7 @@
 
 import java.util.Scanner;
 
- class Star {
+class Star {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
